a332e527bdeb81bfb95b5495feb063b00d1b482c
onlinei2 <onlinei2@onlinei2deimac.lan>
2018-04-18 17:00:57 +0800

    git upload demo

---
+++ gitDemo/gitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by onlinei2 on 2018/4/18.
+//  Copyright © 2018年 onlinei2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"git demo");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
